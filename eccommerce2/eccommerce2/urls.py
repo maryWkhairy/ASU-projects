42d9410ae9269389b89f9b385e8fb4d11a3c2670
Michael Yohanna <73778373+Michael2411@users.noreply.github.com>
2021-01-09 03:27:44 +0200

    Just a trial

--- eccommerce2/eccommerce2/urls.py
+++ eccommerce2/eccommerce2/urls.py
@@ -13,11 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path,include
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path, include
+from . import settings
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('reg.urls')),
-    path('',include('home.urls')),
+    path('', include('home.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- eccommerce2/home/urls.py
+++ eccommerce2/home/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('log/', views.log, name='log'),
     path('reg/', views.reg, name='reg'),
-
+    path('cart/', views.cart, name="cart"),
+    path('dashboard/', views.dashboard, name="dashboard"),
 ]
